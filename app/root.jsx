897ffd8e0b8feb873692ae0946eb20bf776f577f
AdityaChopra18 <adityachopra1808@gmail.com>
2026-08-17 12:29:27 +0530

    Add custom favicon

--- app/root.jsx
+++ app/root.jsx
@@ -17,6 +17,7 @@ export default function App() {
           rel="stylesheet"
           href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
         />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <Meta />
         <Links />
       </head>
